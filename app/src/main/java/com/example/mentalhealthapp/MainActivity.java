c858447d17a155407107794c133191e7b9d0dccb
Rose Mary <rosemary123c@gmail.com>
2020-11-13 09:46:37 +0530

    Test commit

--- app/src/main/java/com/example/mentalhealthapp/MainActivity.java
+++ app/src/main/java/com/example/mentalhealthapp/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.mentalhealthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity
+{
+    Timer timer;
+    String ifName;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ifName= getIntent().getExtras().getString("Name");
+
+        timer= new Timer();
+        timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                //if(ifName.equals(""))
+                {
+                    Intent I1 = new Intent(MainActivity.this, Login.class);
+                    startActivity(I1);
+                    finish();
+                }
+//                else
+//                {
+//                    Intent I2 = new Intent(MainActivity.this, MainMenu.class);
+//                    startActivity(I2);
+//                    finish();
+//                }
+            }
+        },1000);
+
     }
 }
+
